df481b35296fe30b0729b4e0bae946eac6221a56
ahimranVT <abdulhadiimran@gmail.com>
2024-05-14 05:55:02 -0400

    updated the logic to handle chunks greater than the embedding model's context limit

--- helpers.py
+++ helpers.py
@@ -16,14 +16,22 @@ def embed(texts):
 
 # Saves chunks in a dictionary to a csv file
 def save_chunks(chunk_dict, filename):
-    with open(filename, "w", encoding="utf-8", newline='') as file:
-        writer = csv.writer(file)
-        for chunk in chunk_dict:
-            writer.writerow(chunk + '\n')
+    with open(filename, 'w', encoding="utf-8") as f:
+        for chunk in chunk_dict.values():
+            f.write(chunk)
 
 def read_chunks(chunk_dict, filename):
-    with open("text.txt", "r", encoding="utf-8", newline = '') as file:
-        reader = csv.reader(filename)
-        for idx, chunk in enumerate(reader, start=1):
-            chunk_dict[str(idx)] = chunk
+    with open(filename, "r", encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for idx, chunk in enumerate(reader):
+            chunk_dict[str(idx)] = chunk[0]
     return chunk_dict
+
+def segment_chunk(chunk, segment_length):
+    chunk_segments = []
+    for i in range(0, len(chunk), segment_length):
+        start = i
+        end = min(i + segment_length, len(chunk))
+        segment = chunk[start:end]
+        chunk_segments.append(segment)
+    return chunk_segments
